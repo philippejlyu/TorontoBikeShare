1b92e623e3fd13736adbf4181f40304ca285bf21
Philippe Yu <philippejlyu@gmail.com>
2020-08-03 14:38:13 -0700

    Initial Commit

---
+++ TorontoBikeShare WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TorontoBikeShare WatchKit Extension
+//
+//  Created by Philippe Yu on 2020-08-03.
+//  Copyright © 2020 PhilippeYu. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TorontoBikeShare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TorontoBikeShare
+//
+//  Created by Philippe Yu on 2020-08-03.
+//  Copyright © 2020 PhilippeYu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
